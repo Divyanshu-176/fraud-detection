88b8a7d1cce231fa9549cb27b19953e662ce7031
Divyanshu-176 <divyanshujain176@gmail.com>
2026-04-19 15:57:54 +0000

    jenkins test

--- frontend/src/pages/AnalyticsDashboard.jsx
+++ frontend/src/pages/AnalyticsDashboard.jsx
@@ -52,6 +52,7 @@ export default function AnalyticsDashboard() {
 
   return (
     <div className="flex flex-col gap-10">
+      iuhfiuhfiuhfuhfiuh
       <div className="flex flex-col gap-4 sm:flex-row sm:items-end sm:justify-between">
         <div>
           <h1 className="text-2xl font-semibold tracking-tight sm:text-3xl">Analytics</h1>
